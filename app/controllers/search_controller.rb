7660fed1ee78902985290e098d51bab5ff956053
amhursh <amhursh@gmail.com>
2017-10-18 10:44:15 -0600

    Working on nrel api call functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     zip = params[:q]
-    @stations_by_zip = NrelService.new(zip)
+    @stations_by_zip = NrelService.new(zip).get_stations
   end
 
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,9 +1,25 @@
 class NrelService
 
   def initialize(zip)
-    conn = Faraday.new(url: "/api/alt-fuel-stations/v1/") do
-      faraday.headers["api_key"] = 
+    @zip = zip
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/") do |f|
+      # f.headers["api_key"] = ENV["api_key"]
+      f.adapter Faraday.default_adapter
     end
   end
+
+  def get_stations
+    response = conn.get do |req|
+      req.url '/nearest.json'
+      req.params['api_key'] = ENV["api_key"]
+      req.params['location'] = zip
+    end
+    binding.pry
+  end
+
+  private
+
+    attr_reader :zip,
+                :conn
 
 end
