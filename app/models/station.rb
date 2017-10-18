c2090d386c78c31e06b7ac4573d59ea6b82c90b3
amhursh <amhursh@gmail.com>
2017-10-18 11:12:13 -0600

    Add model for station

---
+++ app/models/station.rb
@@ -0,0 +1,27 @@
+class Station
+
+  attr_reader :name,
+              :street,
+              :city,
+              :state,
+              :zip,
+              :fuel_type,
+              :distance,
+              :access_times
+
+  def initialize(input)
+    @name = input[:station_name]
+    @street = input[:street_address]
+    @city = input[:city]
+    @state = input[:state]
+    @zip = input[:zip]
+    @fuel_type = input[:fuel_type_code]
+    @distance = input[:distance]
+    @access_times = input[:access_days_time]
+  end
+
+  def address
+    address + " " + city + " " + state + " " + zip
+  end
+
+end
