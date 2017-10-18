9ae220b61ee1df953f84cf4151fd262a018c9a5c
amhursh <amhursh@gmail.com>
2017-10-18 09:44:50 -0600

    Refactor a few test issues

--- spec/features/user_can_search_stations_by_zip_spec.rb
+++ spec/features/user_can_search_stations_by_zip_spec.rb
@@ -6,7 +6,7 @@ feature "User can search stations" do
     # When I visit "/"
     visit '/'
     # And I fill in the search form with 80203
-    fill_in 'search', with: '80203'
+    fill_in :q, with: '80203'
     # And I click "Locate"
     click_on 'Locate'
     # Then I should be on page "/search" with parameters visible in the url
